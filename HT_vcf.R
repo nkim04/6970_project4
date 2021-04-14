1eeeaa8dc9dc3bb2e49117f7681e91ec589f473f
Heather Treleaven <heathertreleaven@Heathers-MacBook-Pro-5.local>
2021-04-14 02:21:54 -0400

    minor edits

--- HT_vcf.R
+++ HT_vcf.R
@@ -15,35 +15,34 @@ set.seed(4242)
 
 train.index <- sample(1:nrow(counts), round(0.70*nrow(counts),0))
 
-B <- 100
+B <- 300
 
 n <- length(train.index)
 
 L_tree <-  list()
-for(j in 1:100){
+for(j in 1:300){
   idx = sample(train.index, size=n, replace=TRUE)
   L_tree[[j]] = rpart(as.factor(Population)~., counts[idx,],minsplit=1 , cp=0, method = "class")
 }
 
 #Example Decision Trees
-fancyRpartPlot(L_tree[[2]], main = "Bagging Decision Tree for European and East Asian Superpopulations: tree 2", sub = "Using ALDH2, CREB1, OCA2 and SLC45A2 SNPs")
-fancyRpartPlot(L_tree[[5]], main = "Bagging Decision Tree: tree 5", sub = "Using ALDH2, CREB1, OCA2 and SLC45A2 SNPs")
-fancyRpartPlot(L_tree[[17]], main = "Bagging Decision Tree: tree 17", sub = "Using ALDH2, CREB1, OCA2 and SLC45A2 SNPs")
-fancyRpartPlot(L_tree[[75]], main = "Bagging Decision Tree: tree 75", sub = "Using ALDH2, CREB1, OCA2 and SLC45A2 SNPs")
+par(mfrow=c(1,2))
+fancyRpartPlot(L_tree[[30]], main = "Bagging Decision Tree: tree 30", sub = "")
+fancyRpartPlot(L_tree[[275]], main = "Bagging Decision Tree: tree 275", sub = "")
 
 ## Average results
 #predictions on test data for all trees
 predict_test_dat <- function(i){
   predict(L_tree[[i]],newdata=counts[-train.index,],type="prob")[,2]}
 
-out <- lapply(1:100,predict_test_dat)
+out <- lapply(1:300,predict_test_dat)
 
 pred_mat <- matrix(unlist(out),ncol=length(out))
 
 dim(pred_mat) ## 302 observations in the test set!
 
 pred_test <- apply(pred_mat,1,mean)
-
+table(counts[-train.index,]$Population, round(pred_test))
 ## Predictive Performance
 
 bag_roc <- roc(counts$Population[-train.index],pred_test)
@@ -63,8 +62,7 @@ rpartFit <- train(Population ~ ., data = counts[train.index,],
                                trControl = train.control,
                                metric = "ROC"
 )
-
-rpartFit$results
+par(mfrow=c(1,1))
 fancyRpartPlot(rpartFit$finalModel, main = "Single Decision Tree for European and East Asian Superpopulations", sub = "Using ALDH2, CREB1, OCA2 and SLC45A2 SNPs")  
 
 testPrdsTree <- predict(rpartFit$finalModel, counts[-train.index,],type = "class")
@@ -72,10 +70,10 @@ table(counts[-train.index,]$Population, testPrdsTree)
 
 single_roc <- roc(counts$Population[-train.index],as.numeric(testPrdsTree))
 single_roc
-
-bag_roc$auc ; single_roc$auc
+plot(single_roc, main = "AUC for Single Decision Tree")
+single_roc$auc
 
 #Compare AUC Plots
-par(mfrow=c(1,2))
+par(mfrow=c(1,1))
 plot(bag_roc, main = "AUC for Bagging Decision Trees")
-plot(single_roc, main = "AUC for Single Decision Tree")
+bag_roc$auc 
